2b8d7f9b6af5dcb25d74ac0b39cb8e3302e94a19
ittosolution-png <ittosolution@gmail.com>
2026-05-01 12:24:01 +0800

    Update whatsapp.js

--- helpers/whatsapp.js
+++ helpers/whatsapp.js
@@ -9,13 +9,36 @@ async function initWhatsApp(pool) {
     console.log('[WA-LOCAL] Initializing local WhatsApp client...');
     connectionStatus = 'INITIALIZING';
 
+    const fs = require('fs');
+    const paths = [
+        '/usr/bin/google-chrome',
+        '/usr/bin/google-chrome-stable',
+        '/usr/bin/chromium-browser',
+        '/usr/bin/chromium',
+        '/opt/google/chrome/google-chrome'
+    ];
+
+    let chromePath = null;
+    for (const p of paths) {
+        if (fs.existsSync(p)) {
+            chromePath = p;
+            break;
+        }
+    }
+
+    if (chromePath) {
+        console.log('[WA-LOCAL] Found Chrome at:', chromePath);
+    } else {
+        console.error('[WA-LOCAL] Chrome executable NOT FOUND in common paths!');
+    }
+
     client = new Client({
         authStrategy: new LocalAuth({
             dataPath: './.wwebjs_auth'
         }),
         puppeteer: {
             headless: true,
-            executablePath: '/usr/bin/google-chrome', // Path for Google Chrome on Linux
+            executablePath: chromePath,
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
@@ -30,28 +53,38 @@ async function initWhatsApp(pool) {
     });
 
     client.on('qr', (qr) => {
-        console.log('[WA-LOCAL] QR RECEIVED');
+        console.log('[WA-LOCAL] QR RECEIVED! Generating DataURL...');
         qrcode.toDataURL(qr, (err, url) => {
-            qrData = url;
+            if (err) {
+                console.error('[WA-LOCAL] QR DataURL Error:', err.message);
+            } else {
+                qrData = url;
+                console.log('[WA-LOCAL] QR DataURL ready to be displayed.');
+            }
         });
         connectionStatus = 'DISCONNECTED';
     });
 
     client.on('authenticated', () => {
-        console.log('[WA-LOCAL] AUTHENTICATED');
+        console.log('[WA-LOCAL] AUTHENTICATED SUCCESS! Session saved.');
         connectionStatus = 'AUTHENTICATED';
         qrData = null;
     });
 
     client.on('auth_failure', msg => {
-        console.error('[WA-LOCAL] AUTHENTICATION FAILURE', msg);
+        console.error('[WA-LOCAL] AUTHENTICATION FAILURE:', msg);
         connectionStatus = 'DISCONNECTED';
     });
 
     client.on('ready', () => {
-        console.log('[WA-LOCAL] READY');
+        console.log('[WA-LOCAL] CLIENT IS READY! You can now send messages.');
         connectionStatus = 'READY';
         qrData = null;
+    });
+
+    client.on('loading_screen', (percent, message) => {
+        console.log('[WA-LOCAL] LOADING:', percent, '%', message);
+        connectionStatus = 'INITIALIZING';
     });
 
     client.on('disconnected', (reason) => {
