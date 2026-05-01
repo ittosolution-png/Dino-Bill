62d84feffa8956e867cdd1a687d2dbf20900b15a
ittosolution-png <ittosolution@gmail.com>
2026-05-01 12:57:00 +0800

    Update whatsapp.js

--- helpers/whatsapp.js
+++ helpers/whatsapp.js
@@ -47,7 +47,9 @@ async function initWhatsApp(pool) {
                 '--no-first-run',
                 '--no-zygote',
                 '--single-process',
-                '--disable-gpu'
+                '--disable-gpu',
+                '--disable-extensions',
+                '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
             ]
         }
     });
